79a019f93e84ac87ee5b28eb83e039a45d628984
Sergio Arbeo <serabe@gmail.com>
2014-11-19 00:32:36 +0100

    Add description to zones.

--- server/mocks/zones.js
+++ server/mocks/zones.js
@@ -16,67 +16,78 @@ var ZONES =
     id: 'castolia',
     name: 'Castolia',
     main: true,
-    borders: ['pastollanos-este']
+    borders: ['pastollanos-este'],
+    description: 'Ciudad inicial bajo el dominio del rey Reinaldo'
 },
 {
     id: 'portopolis',
     name: 'Portópolis',
     main: true,
-    borders: ['pastollanos-oeste', 'archipielago-tortuga']
+    borders: ['pastollanos-oeste', 'archipielago-tortuga'],
+    description: 'Preciosa ciudad costera llena de piratas.'
 },
 {
     id: 'terramago',
     name: 'Terramago',
     main: true,
-    borders: ['desfiladero-desertico']
+    borders: ['desfiladero-desertico'],
+    description: 'Temible zona donde reina el sultanieblo.'
 },
 {
     id: 'pastollanos-este',
     name: 'Este de Pastollanos',
     main: false,
-    borders: ['castolia','pastollanos-oeste', 'monteniveo']
+    borders: ['castolia','pastollanos-oeste', 'monteniveo'],
+    description: 'Primera zona con enemigos: coyotes, abejas, ovejas y bandidos.'
 },
 {
     id: 'monteniveo',
     name: 'Monteníveo',
     main: false,
-    borders: ['pastollanos-este']
+    borders: ['pastollanos-este'],
+    description: 'Monte donde podrás encontrar a la única persona maestra en todas las vidas.'
 },
 {
     id: 'cima-monteniveo',
     name: 'Cima de Monteníveo',
     main: false,
-    borders: ['monteniveo', 'cueva-somnidragon']
+    borders: ['monteniveo', 'cueva-somnidragon'],
+    description: 'Cima nevada con tigres, avestruces, conejos y Tyranosaurus Rex'
 },
 {
     id: 'cueva-somnidragon',
-    name: 'Cueva deñ Somnidragón',
+    name: 'Cueva del Somnidragón',
     main: false,
-    borders: ['cima-monteniveo']
+    borders: ['cima-monteniveo'],
+    description: 'Cueva con las crías del Somnidragón: los dragones hadas.'
 },
 {
     id: 'pastollanos-oeste',
     name: 'Oeste de Pastollanos',
     main: false,
-    borders: ['pastollanos-este', 'granja-rotrigo', 'portopolis', 'desfiladero-desertico']
+    borders: ['pastollanos-este', 'granja-rotrigo', 'portopolis', 'desfiladero-desertico'],
+    description: 'Zona oeste de Pastollanos, con serpientes, sapos, toros y hortalizas asesinas.'
 },
 {
     id: 'granja-rotrigo',
     name: 'Granja de Rotrigo',
     main: false,
-    borders: ['pastollanos-oeste']
+    borders: ['pastollanos-oeste'],
+    description: 'Preciosa granja en medio del oeste de Pastollanos'
 },
 {
     id: 'archipielago-tortuga',
     name: 'Archipiélago Tortuga',
     main: false,
-    borders: ['portopolis']
+    borders: ['portopolis'],
+    description: 'Zona paradisiaca al lado de Portópolis de no ser por sus pirañas y piratas zombies.'
 },
 {
     id: 'desfiladero-desertico',
     name: 'Desfiladero desértico',
     main: false,
-    borders: ['pastollanos-oeste', 'terramago']
+    borders: ['pastollanos-oeste', 'terramago'],
+    description: 'Una zona de paso que conduce a la peligrosa Terramago'
 }
 ];
 
@@ -101,7 +112,8 @@ ID_TO_ZONE['missingno'] = [{
     id: 'missingno',
     name: 'Missingno',
     main: false,
-    borders: []
+    borders: [],
+    description: 'Una rara zona desconocida'
 }];
 
 var MAIN_ZONES = ZONES.filter(function(el) {
